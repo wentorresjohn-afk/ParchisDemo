9f5dd1eb67e4d8027f9395f8420810ddb7d20127
wentorresjohn-afk <wentorresjohn@gmail.com>
2025-10-17 14:12:03 -0600

    first commit

---
+++ src/main/java/Main/ParchisDemo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+import View.GUIPrincipal;
+
+/**
+ *
+ * @author jhonw
+ */
+public class ParchisDemo 
+{
+    public static void main(String[] args) 
+    {
+        GUIPrincipal v1 = new GUIPrincipal();
+        v1.setVisible(true);
+        v1.setLocationRelativeTo(null);
+    }
+}
