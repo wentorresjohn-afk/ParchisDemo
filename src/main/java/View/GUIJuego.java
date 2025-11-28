0bf10f69b9a860374486cfe707c0f6f8362c6846
wentorresjohn-afk <wentorresjohn@gmail.com>
2025-11-27 21:47:06 -0600

    trabajando en fase 4 y 5, el cronometro ya se muestra, el dado funciona, las fichas aun no se mueven, se encontró un bug a la hora de cambiar entre el jugador y el oponente

--- src/main/java/View/GUIJuego.java
+++ src/main/java/View/GUIJuego.java
@@ -13,22 +13,25 @@ public class GUIJuego extends javax.swing.JFrame {
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(GUIJuego.class.getName());
 
     
-    private PanelControl panelControl = new PanelControl();
-    private PanelTablero panelTablero1 = new PanelTablero();
+//    private PanelControl panelControl = new PanelControl();
+//    private PanelTablero panelTablero1 = new PanelTablero();
     /**
      * Creates new form GUIJuego
      */
-    public GUIJuego() {
+    public GUIJuego() 
+    {
         initComponents();
     }
     
 
-    public PanelControl getPanelControl() {
-        return panelControl;
+    public PanelControl getPanelControl() 
+    {
+        return panelControl1;
     }
 
-    public PanelTablero getPanelTablero1() {
-        return panelTablero1;
+    public PanelTablero getPanelTablero1() 
+    {
+        return panelTablero3;
     }
     
 
@@ -41,8 +44,8 @@ public class GUIJuego extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panelTablero2 = new View.PanelTablero();
         panelControl1 = new View.PanelControl();
+        panelTablero3 = new View.PanelTablero();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Juego");
@@ -52,18 +55,17 @@ public class GUIJuego extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(panelTablero2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(panelControl1, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addComponent(panelTablero3, javax.swing.GroupLayout.PREFERRED_SIZE, 501, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelControl1, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(panelTablero2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(panelControl1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(panelTablero3, javax.swing.GroupLayout.DEFAULT_SIZE, 505, Short.MAX_VALUE)
+                    .addComponent(panelControl1, javax.swing.GroupLayout.DEFAULT_SIZE, 505, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -72,6 +74,6 @@ public class GUIJuego extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private View.PanelControl panelControl1;
-    private View.PanelTablero panelTablero2;
+    private View.PanelTablero panelTablero3;
     // End of variables declaration//GEN-END:variables
 }
