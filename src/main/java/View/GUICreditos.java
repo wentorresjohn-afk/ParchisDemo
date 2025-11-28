ae98720118d608ca41a94fe7399764a75d0b7f6f
wentorresjohn-afk <wentorresjohn@gmail.com>
2025-11-28 11:24:03 -0600

    Las fichas se mueven, entran en las casillas de win, los GUI credito, instrucciones e historia están listos

--- src/main/java/View/GUICreditos.java
+++ src/main/java/View/GUICreditos.java
@@ -28,18 +28,22 @@ public class GUICreditos extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Créditos");
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Creditos.png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jLabel1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 427, Short.MAX_VALUE)
         );
 
         pack();
@@ -50,5 +54,6 @@ public class GUICreditos extends javax.swing.JFrame {
      */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
